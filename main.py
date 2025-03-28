5e7e0a8bf80d3f29a43cc9ee12f20200318a2240
Cody <cchronister@protonmail.com>
2025-03-28 07:58:32 -0500

    Test

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ import random
 import math
 
 """
+Breakout:
+    This game of Breakout is built using tkinter canvas and written before the author learns classes.
+    Essentially, the trick to making this work was to use a dictionary to store the coordinates of the ball
+    and update that dictionary after each move. This prevents entire PILES of arguments needing to be passed
+    to and returned from all the functions. Now, we just return the ball and allow the animation loop to handle
+    updating our notes of where it is.
+
 TODO:
-    - update all canvas usages to work in real life. current syntax is for Code in Place.
-    - solve paddle bug. ball should not get stuck inside paddle when entering from side.
+    - add physics to cause ball to change x_velocity when hit from different sides by the paddle
 
-    - use list to pass params to ball related functions, i.e.
+    - solve paddle bug
 
-    def some_func(a_char, a_float, a_something):
-    print a_char
-
-    params = ['a', 3.4, None]
-    some_func(*params)
-
-    >> a
+    - rewrite function for lay_bricks in a way that labels each one on creation. So, brick_0 through brick_99 will be variables.
 """
 
 CANVAS_WIDTH = 500
@@ -27,99 +27,210 @@ PADDLE_WIDTH = 80
 PADDLE_HEIGHT = 15
 
 BALL_DIAMETER = 20
+BALL_RADIUS = 10
 
 BRICK_GAP = 5
 BRICK_WIDTH = (CANVAS_WIDTH - BRICK_GAP * 9) / 10
 BRICK_HEIGHT = 10
 
-DELAY = 0.05
+DELAY = 0.01
+
+ball_params = {"bx1": None, "by1": None, "bx2": None, "by2": None}
+paddle_params = {"px1": None, "py1": None, "px2": None, "py2": None}
+ball_coords = ball_params.values()  # only used in canvas.find_overlapping(*ball_coords) b/c we need to unpack the dictionaries values to pass into canvas
+paddle_coords = paddle_params.values()
 
 
 def main():
-    canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
-    lay_bricks(canvas)
+    # Backend setup.
+    num_lst = init_num_lst()
     num_bricks = 100
     lives = 3
-    paddle = place_paddle(canvas)  # shape_100
-    ball = place_ball(canvas)  # shape_101
-    lives_left = life_tracker(canvas, lives)  # shape_102
 
-    ball_left_x = canvas.get_left_x(ball)
-    ball_top_y = canvas.get_top_y(ball)
-    x_velocity = random_excluding_zero()
+    # Setup board
+    canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
+    lay_bricks(canvas)
+    paddle = place_paddle(canvas)
+    ball = place_ball(canvas)
+    ball_params = update_ball_params(canvas, ball)  # get params for ball now that it exists
+    paddle_params = update_paddle_params(canvas, paddle)  # get params for paddle now that it exists
+    lives_left = life_tracker(canvas, lives)
+
+    x_velocity = random_excluding_zero(num_lst)
     y_velocity = 10
 
     while num_bricks > 0 and lives > 0:  # animation loop
 
+        ### Check ###
         # check for collisions with safe walls
         x_velocity, y_velocity = walls_check(
-            ball_left_x,
-            ball_top_y,
             x_velocity,
             y_velocity
         )
 
-        # check for collisions with other canvas objects once per loop
-        y_velocity, num_bricks = collision_check(
+        ### Check ###
+        # check for collisions with other canvas objects, minus the paddle and walls once per loop
+        x_velocity, y_velocity, num_bricks = collision_check(
             canvas,
-            ball_left_x, ball_top_y,
-            x_velocity, y_velocity,
-            num_bricks
+            paddle,
+            x_velocity,
+            y_velocity,
+            num_bricks,
         )
 
+        ### Check ###
         # Ball touches bottom wall. Lose a life, return to center.
-        ball_left_x, ball_top_y, x_velocity, lives, lives_left = bottom_wall_check(
+        ball, lives, lives_left = bottom_wall_check(
             canvas,
             ball,
-            ball_left_x,
-            ball_top_y,
-            x_velocity,
             lives,
             lives_left
         )
 
-        # Ball continues moving in straight line
-        ball_left_x += x_velocity
-        ball_top_y += y_velocity
-        canvas.moveto(ball, ball_left_x, ball_top_y)
-
+        ### Animation ###
         # Mouse tracking for paddle
         mouse_x = canvas.get_mouse_x()
-        canvas.moveto(paddle, mouse_x, PADDLE_Y)
+        canvas.moveto(paddle, mouse_x - PADDLE_WIDTH / 2, PADDLE_Y)
+        paddle_params = update_paddle_params(canvas, paddle)
+
+        ### Paddle collision check ###
+        # This goes here because we are checking the paddle coords for collisions rather than the ball coords.
+        x_velocity, y_velocity = paddle_collision_check(canvas, paddle, ball, x_velocity, y_velocity)
+
+        ### Animation ###
+        # Ball continues moving in straight line
+        ball_params = update_ball_params(canvas, ball)
+        ball_params["bx1"] = ball_params["bx1"] + x_velocity
+        ball_params["by1"] = ball_params["by1"] + y_velocity
+        canvas.moveto(ball, ball_params["bx1"], ball_params["by1"])
+        ball_params = update_ball_params(canvas, ball)
+
         time.sleep(DELAY)
 
     end_game(canvas, ball, num_bricks)
 
 
-def bottom_wall_check(canvas, ball, ball_left_x, ball_top_y, x_velocity, lives, lives_left):
-    if (ball_top_y + BALL_DIAMETER) >= CANVAS_HEIGHT:
+def bottom_wall_check(canvas, ball, lives, lives_left):
+    """
+    If the ball touches (bottom_y greater than or equal to) the bottom wall, update lives and print new lives_left,
+    generate new ball angle, and return the ball to center of screen.
+    """
+    if ball_touches_bottom_wall():
         lives, lives_left = update_lives(canvas, lives, lives_left)
-        x_velocity = random_excluding_zero()  # reset x_velocity as if the game started from scratch
-        ball_left_x = CANVAS_WIDTH / 2 - BALL_DIAMETER  # return ball to center
-        ball_top_y = CANVAS_HEIGHT / 2 - BALL_DIAMETER
-        canvas.moveto(ball, ball_left_x, ball_top_y)
-        # Not returning y_velocity b/c it will always be down in this case and
-        # we don't need to change that on reset.
-    return ball_left_x, ball_top_y, x_velocity, lives, lives_left
+        canvas.delete(ball)
+        ball = place_ball(canvas)
+
+    return ball, lives, lives_left
 
 
-def walls_check(ball_left_x, ball_top_y, x_velocity, y_velocity):
-    if ball_left_x < 0 or (ball_left_x + BALL_DIAMETER) > CANVAS_WIDTH:  # check left and right walls
+def walls_check(x_velocity, y_velocity):
+    """
+    If ball touches a wall that isn't the bottom one, bounce it by flipping the x or y velocity depending on which wall it hits.
+    """
+    if ball_params["bx1"] <= 0 or ball_params["bx2"] >= CANVAS_WIDTH:  # check left and right walls
         x_velocity = -x_velocity
-    elif ball_top_y < 0:  # check top wall
+    elif ball_params["by1"] <= 0:  # check top wall
         y_velocity = -y_velocity
+
     return x_velocity, y_velocity
 
 
-def random_excluding_zero():
-    rand_int = random.randint(-10, 10)
-    while rand_int == 0:
-        rand_int = random.randint(-10, 10)
-    return rand_int
+def collision_check(canvas, paddle, x_velocity, y_velocity, num_bricks):
+    """
+    Creates a list of overlapping objects then if there is more than one object in list (there will always be one, the ball),
+    check if the other object is the paddle, if not, delete it.
+    """
+    overlapping = canvas.find_overlapping(*ball_coords)
+    if len(overlapping) > 1:
+        if paddle not in overlapping:
+            y_velocity = -y_velocity
+            canvas.delete(overlapping[0])
+            num_bricks -= 1
+
+    return x_velocity, y_velocity, num_bricks
+
+
+def paddle_collision_check(canvas, paddle, ball, x_velocity, y_velocity):
+    """
+    Check for collisions with the ball from the paddle's p.o.v. We want to see if checking for paddle bounces after animating
+    the paddle but before animating the ball will solve the stuck_inside_paddle bug
+
+    *This partially worked. The ball still gets stuck when you flick it juuust right really really fast.
+    """
+    overlapping = canvas.find_overlapping(*paddle_coords)
+    if len(overlapping) > 1:
+        if ball in overlapping:
+            y_velocity = -y_velocity
+
+    return x_velocity, y_velocity
+
+def ball_touches_bottom_wall():
+    """
+    if bottom of ball touches bottom wall of canvas, return True.
+    """
+    if ball_params["by2"] >= CANVAS_HEIGHT:
+        return True
+
+
+def update_ball_params(canvas, ball):
+    """
+    Get coords of ball and update values of keys in ball_params dict.
+    """
+    x1, y1 = get_coords(canvas, ball)
+    x2, y2 = (x1 + BALL_DIAMETER), (y1 + BALL_DIAMETER)
+
+    ball_params["bx1"] = x1
+    ball_params["by1"] = y1
+    ball_params["bx2"] = x2
+    ball_params["by2"] = y2
+
+    return ball_params
+
+
+def update_paddle_params(canvas, paddle):
+    """
+    Get coords of paddle and update paddle dict with new values.
+    """
+    x1, y1 = get_coords(canvas, paddle)
+    x2, y2 = (x1 + PADDLE_WIDTH), (y1 + PADDLE_HEIGHT)
+
+    paddle_params["px1"] = x1
+    paddle_params["py1"] = y1
+    paddle_params["px2"] = x2
+    paddle_params["py2"] = y2
+
+    return paddle_params
+
+
+def get_coords(canvas, obj):
+    x, y = canvas.get_left_x(obj), canvas.get_top_y(obj)
+    return x, y
+
+
+def random_excluding_zero(num_lst):
+    """
+    Only used to shoot the ball at a new angle. Roll random ints between -10 and 10. The list excludes 0.
+    """
+    result = random.choice(num_lst)
+    return result
+
+
+def init_num_lst():
+    """
+    Only used for radnom_excluding_zero(num_lst). Creates a list of ints from -10 to 10 without zero in it.
+    """
+    num_lst = []
+    for i in range(-10, 11):
+        if i != 0:
+            num_lst.append(i)
+    return num_lst
 
 
 def life_tracker(canvas, lives):
-    lives_left = canvas.create_text(
+    """
+    Creates a new lives left text with updated number.
+    """
+    lives_left = canvas.create_text(  # The actual obj on screen is lives_left
         CANVAS_WIDTH - 50,
         CANVAS_HEIGHT - 50,
         text=str(lives),
@@ -131,37 +242,27 @@ def life_tracker(canvas, lives):
 
 
 def update_lives(canvas, lives, lives_left):
+    """
+    Process results of losing a life. Replace canvas obj with updated one.
+    """
     lives -= 1
     canvas.delete(lives_left)
     lives_left = life_tracker(canvas, lives)  # shape_102
+
     return lives, lives_left
 
 
 def place_ball(canvas):
-    ball_left_x = CANVAS_WIDTH / 2 - BALL_DIAMETER / 2
-    ball_top_y = CANVAS_HEIGHT / 2 - BALL_DIAMETER / 2
-    ball_right_x = ball_left_x + BALL_DIAMETER
-    ball_bottom_y = ball_top_y + BALL_DIAMETER
+    """
+    Create canvas obj for the ball at center.
+    """
+    x1, y1 = CANVAS_WIDTH / 2 - BALL_DIAMETER / 2, CANVAS_HEIGHT / 2 - BALL_DIAMETER / 2
+    x2, y2 = x1 + BALL_DIAMETER, y1 + BALL_DIAMETER
 
     ball = canvas.create_oval(
-        ball_left_x,
-        ball_top_y,
-        ball_right_x,
-        ball_bottom_y
+        x1, y1, x2, y2
     )
     return ball
-
-
-def collision_check(canvas, x, y, x_velocity, y_velocity, num_bricks):
-    overlapping = canvas.find_overlapping(x, y, x + BALL_DIAMETER, y + BALL_DIAMETER)
-    if len(overlapping) > 1:
-        if overlapping[0] != 'shape_100':
-            y_velocity = -y_velocity
-            canvas.delete(overlapping[0])
-            num_bricks -= 1
-        elif overlapping[0] == 'shape_100':
-            y_velocity = -y_velocity
-    return y_velocity, num_bricks
 
 
 def place_paddle(canvas):
@@ -186,16 +287,16 @@ def lay_brick_row(canvas, brick_top_y, color):
     Prints a single row of bricks left to right. Accepts variables for a new
     row and color each time it runs.
     """
-    for r in range(10):
+    for i in range(10):
         brick_left_x = 0
         brick_right_x = brick_left_x + BRICK_WIDTH
         brick_bottom_y = brick_top_y + BRICK_HEIGHT
         # Just going across one row here (only manipulating x values). New rows handled in lay_bricks.
         canvas.create_rectangle(
-            brick_left_x + (BRICK_WIDTH + BRICK_GAP) * r,
-            # refers to r because bricks are laid along x axis r number of times.
+            brick_left_x + (BRICK_WIDTH + BRICK_GAP) * i,
+            # refers to i because bricks are laid along x axis i number of times.
             brick_top_y,
-            brick_right_x + (BRICK_WIDTH + BRICK_GAP) * r,
+            brick_right_x + (BRICK_WIDTH + BRICK_GAP) * i,
             # this one has to match the operations on left_x because if it doesn't the bricks end up upside down.
             brick_bottom_y,
             color
@@ -208,14 +309,14 @@ def lay_bricks(canvas):
     function: two rows red, two orange, etc.
     """
     for i in range(10):
-        brick_top_y = i * (BRICK_HEIGHT + BRICK_GAP)
+        brick_top_y = i * (BRICK_HEIGHT + BRICK_GAP)  # first instance of brick_top_y variable
         if i <= 1:
             lay_brick_row(canvas, brick_top_y, 'red')
-        elif i == 2 or i == 3:
+        elif 2 <= i <= 3:
             lay_brick_row(canvas, brick_top_y, 'orange')
-        elif i == 4 or i == 5:
+        elif 4 <= i <= 5:
             lay_brick_row(canvas, brick_top_y, 'yellow')
-        elif i == 6 or i == 7:
+        elif 6 <= i <= 7:
             lay_brick_row(canvas, brick_top_y, 'lime')
         elif i >= 8:
             lay_brick_row(canvas, brick_top_y, 'cyan')
